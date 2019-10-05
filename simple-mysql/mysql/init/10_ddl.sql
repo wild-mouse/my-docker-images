03bb9b6f30d66a07981a7431d6c1fda6eafae954
Tsukasa Noguchi <wildmouse0813@gmail.com>
2019-10-05 21:43:11 +0900

    Update DDL of tasks table

--- simple-mysql/mysql/init/10_ddl.sql
+++ simple-mysql/mysql/init/10_ddl.sql
@@ -1,7 +1,8 @@
 CREATE DATABASE IF NOT EXISTS playground;
 
 CREATE TABLE playground.tasks(
-  `id` int(11) NOT NULL,
-  `name` varchar(100) NOT NULL
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) NOT NULL,
+  PRIMARY KEY(id)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
