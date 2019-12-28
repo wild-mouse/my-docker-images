09c5043cffe8f6bac46df86bf4914645cf694dd2
Tsukasa Noguchi <wildmouse0813@gmail.com>
2019-12-28 12:11:00 +0900

    Add hello world table

--- simple-mysql/mysql/init/10_ddl.sql
+++ simple-mysql/mysql/init/10_ddl.sql
@@ -1,8 +1,18 @@
 CREATE DATABASE IF NOT EXISTS playground;
 
-CREATE TABLE playground.tasks(
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(100) NOT NULL,
-  PRIMARY KEY(id)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+CREATE TABLE playground.tasks
+(
+    `id`   int(11)      NOT NULL AUTO_INCREMENT,
+    `name` varchar(100) NOT NULL,
+    PRIMARY KEY (id)
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8mb4;
+
+CREATE TABLE playground.hello_worlds
+(
+    `id`   int(11)      NOT NULL AUTO_INCREMENT,
+    `message` varchar(100) NOT NULL DEFAULT 'Hello, World!',
+    PRIMARY KEY (id)
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8mb4;
 
